47399c8781cbbd0f1eb6b832e965dade1e420904
Satish Chandra Nagaraj <Satish.Nagaraj@smarsh.com>
2019-12-24 15:30:07 +0530

    Some java file

---
+++ MemoryLeakGenerator.java
@@ -0,0 +1,20 @@
+package com.smarsh.perftraining.examples;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class MemoryLeakGenerator {
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        Map sampleMap = new HashMap();
+        Thread.sleep(10000);
+        // Loop forever...
+        while(true) {
+            // Create and store a random pair.
+            int randomValue = random.nextInt();
+            sampleMap.put(randomValue, String.valueOf(randomValue));
+            //Thread.sleep(1);
+        }
+    }
+}
+
